7f5489728ca4b5b656406408870a1575c0c23c18
Caz <caz@pukka-j.net>
2022-06-28 17:03:00 +0100

    Update tests to take array of sizes instead of single value

--- CourierKata.Tests/ParcelTests.cs
+++ CourierKata.Tests/ParcelTests.cs
@@ -5,26 +5,26 @@ namespace CourierKata.Tests
 {
     public class ParcelTests
     {
-        [TestCase(1, "Small")]
-        [TestCase(9.9, "Small")]
-        [TestCase(10, "Medium")]
-        [TestCase(49.9, "Medium")]
-        [TestCase(50, "Large")]
-        [TestCase(99.999, "Large")]
-        [TestCase(100, "XL")]
-        [TestCase(1000, "XL")]
-        public void CreateParcel_WhenDataIsValid_ExpectValidParcelType(double size, string expectedParcelCategory)
+        [TestCase(new double[] { 1, 1, 1 }, "Small")]
+        [TestCase(new double[] { 9.9, 9.9, 9.9 }, "Small")]
+        [TestCase(new double[] { 10, 10, 10 }, "Medium")]
+        [TestCase(new double[] { 49.9, 49.9, 49.9 }, "Medium")]
+        [TestCase(new double[] { 50, 50, 50 }, "Large")]
+        [TestCase(new double[] { 99.999, 99.999, 99.999 }, "Large")]
+        [TestCase(new double[] { 100, 100, 100 }, "XL")]
+        [TestCase(new double[] { 1000, 1000, 1000 }, "XL")]
+        public void CreateParcel_WhenDataIsValid_ExpectValidParcelType(double[] sizes, string expectedParcelCategory)
         {
-            Parcel parcel = new Parcel(size);
+            Parcel parcel = new Parcel(sizes);
 
             Assert.AreEqual(expectedParcelCategory, parcel.Category.Name);
         }
 
-        [TestCase(0, "Size is not a valid value.")]
-        [TestCase(-100, "Size is not a valid value.")]
-        public void CreateParcel_WhenDataIsInvalid_ExpectException(double size, string expectedExceptionMessage)
+        [TestCase(new double[] { 0, 0, 0 }, "Size is not a valid value.")]
+        [TestCase(new double[] { -3, -6, -100 }, "Size is not a valid value.")]
+        public void CreateParcel_WhenDataIsInvalid_ExpectException(double[] sizes, string expectedExceptionMessage)
         {
-            var ex = Assert.Throws<ArgumentException>(() => new Parcel(size));
+            var ex = Assert.Throws<ArgumentException>(() => new Parcel(sizes));
             Assert.AreEqual(expectedExceptionMessage, ex?.Message);
         }
     }
