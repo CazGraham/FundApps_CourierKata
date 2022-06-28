e357116a59ee95c53d9c4863320bd5b35521ce49
Caz <caz@pukka-j.net>
2022-06-28 17:20:08 +0100

    Add in Order class to allow multiple parcels and shipping per order.

---
+++ CourierKata/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourierKata
+{
+    public class Order
+    {
+        private const double SpeedyShipmentMultiplier = 2;
+        public bool IsSpeedyShipment { get; }
+        public IEnumerable<Parcel> Parcels { get; }
+
+        public Order(IEnumerable<Parcel> parcels, bool isSpeedyShipment)
+        {
+            if (!parcels.Any())
+            {
+                throw new ArgumentException("Parcel list is empty.");
+            }
+
+            Parcels = parcels;
+            IsSpeedyShipment = isSpeedyShipment;
+        }
+    }
+}
