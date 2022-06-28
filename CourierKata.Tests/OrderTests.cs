b1cda6791c64cdab0146222730933f90c44d3628
Caz <caz@pukka-j.net>
2022-06-28 19:58:06 +0100

    Added additional tests for order to cover shipping costs

--- CourierKata.Tests/OrderTests.cs
+++ CourierKata.Tests/OrderTests.cs
@@ -12,6 +12,11 @@ namespace CourierKata.Tests
         private static List<Parcel> _singleParcelList = new List<Parcel> { _smallParcel };
         private static List<Parcel> _multipleParcelList = new List<Parcel> { _smallParcel, _smallParcel, _smallParcel, _mediumParcel, _mediumParcel };
 
+        private static readonly object[] _singleParcelOrderStandardShipping = { new object[] { new Order(_singleParcelList, false), 3 } };
+        private static readonly object[] _multipleParcelOrderStandardShipping = { new object[] { new Order(_multipleParcelList, false), 25 } };
+        private static readonly object[] _singleParcelOrderSpeedyShipping = { new object[] { new Order(_singleParcelList, true), 6 } };
+        private static readonly object[] _multipleParcelOrderSpeedyShipping = { new object[] { new Order(_multipleParcelList, true), 50 } };
+
         [Test]
         public void CreateOrder_WithSingleParcel_ExpectCorrectParcelList()
         {
@@ -33,6 +38,30 @@ namespace CourierKata.Tests
         {
             var ex = Assert.Throws<ArgumentException>(() => new Order(new List<Parcel> { }, false));
             Assert.AreEqual("Parcel list is empty.", ex?.Message);
+        }
+
+        [TestCaseSource(nameof(_singleParcelOrderStandardShipping))]
+        public void TotalCost_WithSingleParcelOrderStandardShipping_ExpectCorrectTotal(Order order, double expectedTotal)
+        {
+            Assert.AreEqual(expectedTotal, order.TotalCost());
+        }
+
+        [TestCaseSource(nameof(_multipleParcelOrderStandardShipping))]
+        public void TotalCost_WithMultipleParcelOrderStandardShipping_ExpectCorrectTotal(Order order, double expectedTotal)
+        {
+            Assert.AreEqual(expectedTotal, order.TotalCost());
+        }
+
+        [TestCaseSource(nameof(_singleParcelOrderSpeedyShipping))]
+        public void TotalCost_WithSingleParcelOrderSpeedyShipping_ExpectCorrectTotal(Order order, double expectedTotal)
+        {
+            Assert.AreEqual(expectedTotal, order.TotalCost());
+        }
+
+        [TestCaseSource(nameof(_multipleParcelOrderSpeedyShipping))]
+        public void TotalCost_WithMultipleParcelOrderSpeedyShipping_ExpectCorrectTotal(Order order, double expectedTotal)
+        {
+            Assert.AreEqual(expectedTotal, order.TotalCost());
         }
     }
 }
